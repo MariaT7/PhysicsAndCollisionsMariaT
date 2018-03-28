fb44adfa3b7a9dbdae06c5288a9bc00de17d3481
Maria T <maria.temu@ocsbstudent.ca>
2018-03-28 11:07:20 -0400

    Write out

--- main.lua
+++ main.lua
@@ -1,7 +1,99 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- -- Title: PhysicsAndCollisions
+-- Name: Maria T
+-- Course: ICS2O/3C
+-- This program...
+---------------------------------------------------------------------
 
--- Your code here
+--Hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--load physics
+local physics = require("physics")
+
+-- Start physics
+physics.start()
+-----------------------------------------------------------
+--Objects
+------------------------------------------------------------
+--Ground
+local ground = display.newImage("Images/ground.png", 0, 0)
+
+	--Set the x and y pos
+	ground.x = display.contentCenterX
+	ground.y = display.contentHeight
+
+	--Change the width to be the same as the screen 
+	ground.width = display.contentWidth
+
+	-- Add to physics
+	physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
+
+----------------------------------------------------------------------------------------------
+
+--Create a verticle beam
+local beam = display.newImage("Images/beam.png", 0, 0)
+
+	--set the x and y pos
+	beam.x = display.contentCenterX/5
+	beam.x = display.contentCenterY*1.65
+
+	--set the beam size
+	beam.width = display.contentWidth/2
+	beam.height = display.contentHeight/10
+
+	-- rotate the beam 45 degrees so it is on its angle
+	beam:rotate(45)
+
+	-- send it to the back layer
+	beam:toBack()
+
+	--add to physics
+	physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
+
+	--create bkg
+	local bkg = display.newImage("Images/bkg.png", 0, 0)
+
+		--set the x and y pos
+		bkg.x = display.contentCenterX
+		bkg.y = display.contentCenterY
+
+	 	--set the bkg size
+	 	bkg.width = display.contentWeight
+		bkg.height = display.contentHeight
+
+		-- send to back
+		bkg:toBack()
+
+------------------------------------------------------------------------------------------
+--Functions
+------------------------------------------------------------------------------------------
+
+-- create the first ball
+local function firstBall()
+	-- creating first ball
+	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- add to physics 
+	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
+end
+
+------------------------------------------------------------------------------------------------------------
+
+local function secondBall()
+	-- creating first ball
+	local ball2 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- adding to physics
+	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+
+	-- make it smaller than the original size
+	ball2:scale(0.5, 0.5)
+end
+
+----------------------------------------------------------------------------------------------------------
+-- TIMER DELAYS - call each function after the given millisecond
+----------------------------------------------------------------------------------------------------------
+timer.performWithDelay( 0, firstBall)
+timer.performWithDelay( 500, secondBall)
+
